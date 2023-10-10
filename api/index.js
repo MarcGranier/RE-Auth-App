61eca1c82d3900ebec3525b0bbbb350c4105d87a
Marc Granier <mgranier@comcast.net>
2023-10-10 16:13:35 -0400

    Connected to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,9 +1,16 @@
 import express from 'express';
+import connectDB from '../config/db.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+console.log(process.env.PORT);
 
 const app = express();
 
-const port = 3000;
+connectDB();
 
-app.listen(3000, () => {
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
 	console.log(`Server is running on port: ${port}`);
 });
